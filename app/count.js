ee96bf08bbf605b4cd465b7bfad08bedd09235eb
dassshha <dzhguleva@mail.ru>
2022-10-05 22:28:40 +0300

    edit count func

--- app/count.js
+++ app/count.js
@@ -2,6 +2,23 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    var current = start;
+    var timerId;
 
+    function go() {
+      console.log(current);
+      if (current === end) {
+        clearInterval(timerId);
+      }
+      current++;
+    }
+
+    go();
+    timerId = setInterval(go, 100);
+    return {
+      cancel: function () {
+        clearInterval(timerId);
+      }
+    };
   }
 };
