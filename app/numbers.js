005f3c449a0a98d0f92f87818cbeb5bad1df7114
dassshha <dzhguleva@mail.ru>
2022-10-10 13:23:31 +0300

    edit numbers funcs

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,21 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    return Number(parseInt(num, 10).toString(2).
+    split('').
+    reverse()[bit - 1]);
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
-
+    var binary = parseInt(num, 10).toString(2);
+    return binary.padStart(8, '0');
   },
 
   multiply: function(a, b) {
-
+    return parseFloat(a * b);
   }
 };
