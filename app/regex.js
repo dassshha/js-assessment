df3a82e3d5452859e6a819570a6b9f28331a1628
dassshha <dzhguleva@mail.ru>
2022-10-11 14:06:31 +0300

    edit regexp funcs

--- app/regex.js
+++ app/regex.js
@@ -2,26 +2,33 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
   containsNumber: function(str) {
-
+    var reg = /\d/g;
+    return reg.test(str);
   },
 
   containsRepeatingLetter: function(str) {
-
+    var reg = /([a-z])\1/gi;
+    return reg.test(str);
   },
 
   endsWithVowel: function(str) {
-
+    var reg = /[aeiouy]$/gi;
+    return reg.test(str);
   },
 
   captureThreeNumbers: function(str) {
-
+    var reg = /\d{3}/;
+    var matches = str.match(reg) || [];
+    return matches.length ? matches[0] : false;
   },
 
   matchesPattern: function(str) {
-
+    var reg = /^\d{3}-\d{3}-\d{4}$/g;
+    return reg.test(str);
   },
 
   isUSD: function(str) {
-
+    var reg = /^\$\d{1,3}(,\d{3})*(\.\d{2})?$/;
+    return reg.test(str);
   }
 };
